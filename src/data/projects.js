7e4b1b9328a07a12c1706d93577f2b4cfea0d627
Teo Hedelin <teohedelin@MacBook-Pro-som-tillhor-Teo.local>
2026-03-26 22:40:11 +0100

    added project and fixed images

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,7 +6,7 @@ const projects = [
       "Master thesis at Voysys. Implementation of a real-time gaussian splatter algorithm in C++ using OpenGL and CUDA.",
     tags: ["C++", "OpenGL", "CUDA", "Python"],
     color: "#b85f33",
-    link: "#",
+    link: "https://github.com/McSteken/Real-time-Gaussian-Splatting",
     images: [
       { src: "/Images/GS/splat.png", caption: "Final splat" },
       { src: "/Images/GS/pc.png", caption: "Point cloud visualization" },
@@ -30,6 +30,18 @@ const projects = [
   },
   {
     id: 3,
+    title: "Raytracer built in C++",
+    description:
+      "A raytracer built in C++ using OpenGL. Developed in C++",
+    tags: ["C++"],
+    color: "#1c4a5c",
+    link: "https://github.com/Hermelin17/Raytracer",
+    images: [
+      { src: "/Images/ray/room.png", caption: "Final render of the scene" },
+    ],
+  },
+  {
+    id: 4,
     title: "React website for card game",
     description:
       "A React website for a card game where you can create your own decks and play against other players. Developed in React and Node.js.",
@@ -43,13 +55,13 @@ const projects = [
     ],
   },
   {
-    id: 4,
+    id: 5,
     title: "React component for credit card input",
     description:
       "A React component for credit card input. Developed in React and Node.js.",
     tags: ["React", "Node.js"],
     color: "#7d2a91",
-    link: "https://github.com/TeoHedelin/Credit-Card-Input",
+    link: "https://github.com/Hermelin17/Hand-in/tree/main/Lab2",
     images: [
       { src: "/Images/kort/start.png", caption: "Empty form", tall: true },
       { src: "/Images/kort/input.png", caption: "Filled card information, logo in top right corner changes depending on card manufacturer", tall: true },
@@ -57,13 +69,13 @@ const projects = [
     ],
   },
   {
-    id: 5,
+    id: 6,
     title: "Information visualization project",
     description:
       "Visualization of star wars characters and their interactions.",
     tags: ["HTML", "CSS", "JavaScript"],
     color: "#b0ad4a",
-    link: "https://github.com/TeoHedelin/Credit-Card-Input",
+    link: "https://github.com/Hermelin17/TNM111/tree/main/Assignment%204",
     images: [
       { src: "/Images/starwars/anakin.png", caption: "Hovering over Anakin node shows information about the character" },
       { src: "/Images/starwars/AniObi.png", caption: "Hovering over the line between Anakin and Obi-Wan shows the number of interactions between the characters" },
@@ -71,13 +83,13 @@ const projects = [
     ],
   },
   {
-    id: 6,
+    id: 7,
     title: "Information visualization project",
     description:
       "Visualization of recorded earthquakes around the world. The slider can be used to select a specific time period.",
     tags: ["HTML", "CSS", "JavaScript"],
     color: "#61caff",
-    link: "https://github.com/TeoHedelin/Credit-Card-Input",
+    link: "https://github.com/Hermelin17/TNM111/tree/main/Assignment%203",
     images: [
       { src: "/Images/eq/overview.png", caption: "Startpage " },
       { src: "/Images/eq/select.png", caption: "Selecting a earthquake in the chart and seeing the details" },
